b69123a010622a4f209b070b3e31a7a5f6dc2baf
louisevst <louisevassart@gmail.com>
2023-04-24 12:11:10 +0200

    added db connection

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,24 @@
 import express, { Express, Request, Response } from "express";
+import mongoose, { MongooseOptions } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+const password = process.env.MONGODB_PASSWORD;
 const app: Express = express();
+
+const options: MongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as MongooseOptions;
+
+mongoose
+  .connect(
+    `mongodb+srv://louisevassart:${password}@cluster.xltoske.mongodb.net/?retryWrites=true&w=majority`,
+    options
+  )
+
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
   console.log("Requête reçue !");
--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,7 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const password = process.env.MONGODB_PASSWORD;
 const app = (0, express_1.default)();
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+mongoose_1.default
+    .connect(`mongodb+srv://louisevassart:${password}@cluster.xltoske.mongodb.net/?retryWrites=true&w=majority`, options)
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
 app.use((req, res, next) => {
     console.log("Requête reçue !");
     next();
